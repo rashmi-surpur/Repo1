532c0319aeb6acb1cbe4be2dce6cd6aab51c0655
Rashmi R S <rashmirangana.surpu@hcl.com>
2024-11-22 11:27:57 +0530

    Test Controller added newly by Rashmi

---
+++ GitExample3/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitExample3.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Index -V1.0");
+        }
+    }
+}
