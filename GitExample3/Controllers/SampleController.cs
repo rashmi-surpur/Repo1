8ed282d6d651a98fe9e8860e5a18a3b878e5ed83
Rashmi R S <rashmirangana.surpu@hcl.com>
2024-11-22 11:33:57 +0530

    New Controller Added

---
+++ GitExample3/Controllers/SampleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitExample3.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Index-V1.1");
+        }
+    }
+}
